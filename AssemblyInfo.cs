6157e6b15fc1fff3c751f78db85c50040384bf70
damngox <khasasnov95@gmail.com>
2025-03-31 17:39:25 +0300

    Добавил еще
    Assemblinfo

---
+++ AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BlockEnhancementMod-Besiege")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BlockEnhancementMod-Besiege")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("6a16f1f7-31c2-4cae-9fd3-0c56bbd29757")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
